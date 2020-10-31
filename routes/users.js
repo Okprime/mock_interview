7e6faa809ccacb25f93582b16194eaccb860083d
Uwede <ouwede@terragonltd.com>
2020-10-31 10:11:51 +0100

    code update

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,15 @@ const router = express.Router();
 const multer = require('multer');
 const sharp = require('sharp');
 const fs = require('fs');
+// const path = require('path');
 
 const uploadDir = `${process.cwd()}/public/images`;
 console.log('uploadDir', uploadDir);
 
-// if (!fs.existsSync(uploadDir)) {
-//       console.log('!fs.existsSync(uploadDir)', !fs.existsSync(uploadDir));
-//       fs.mkdirSync(uploadDir);
-// }
+if (!fs.existsSync(uploadDir)) {
+      console.log('!fs.existsSync(uploadDir)', !fs.existsSync(uploadDir));
+      fs.mkdirSync(uploadDir);
+}
 
 const storage = multer.diskStorage({
       destination: (req, file, cb) => {
