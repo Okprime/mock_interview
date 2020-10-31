35bfe7647de1f0db443decb04366cf9f8d975c90
Uwede <ouwede@terragonltd.com>
2020-10-31 07:54:29 +0100

    debug

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,9 @@ const router = express.Router();
 const multer = require('multer');
 const sharp = require('sharp');
 const fs = require('fs');
+const path = require("path");
 
-const uploadDir = `${__dirname}/uploads`;
-console.log('upload directory', uploadDir);
-
+const uploadDir = `${process.cwd()}/public/images`;
 if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir);
 }
