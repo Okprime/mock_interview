9816231db70f4cb8ad443039fa4e1255173c5cba
Uwede <ouwede@terragonltd.com>
2020-10-31 08:47:02 +0100

    debug

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const path = require("path");
 
 const uploadDir = `${process.cwd()}/public/images`;
+console.log('uploadDir', uploadDir);
 if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir);
 }
